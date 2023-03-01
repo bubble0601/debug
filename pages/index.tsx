6bcbc0d276ff168506f281937346a40b370c0182
bubble0601 <bubble0601@gmail.com>
2023-03-01 18:58:31 +0900

    dummy

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,9 @@ export default () => (
   <div>
     <h1>Index</h1>
     <p>
+      <a href="/image">Exif orientation</a>
+    </p>
+    <p>
       <a href="/image/device-orientation">Device orientation</a>
     </p>
     <p>
