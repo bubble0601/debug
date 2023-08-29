2e157be80597f7af6f1bff52159bdea5ab64d515
bubble0601 <bubble0601@gmail.com>
2023-08-29 12:59:25 +0900

    better ime support

--- components/editor/ime-support.ts
+++ components/editor/ime-support.ts
@@ -5,6 +5,7 @@ export type IMESupportOptions = {
   types: string[];
 };
 
+// 選択範囲の始点が行の最後(改行文字)のときにIME入力を開始すると、次の行の先頭に1文字入力されてしまう問題の対処
 export const IMESupport = Extension.create<IMESupportOptions>({
   name: "imeSupport",
 
@@ -15,8 +16,9 @@ export const IMESupport = Extension.create<IMESupportOptions>({
         key: new PluginKey("imeSupport"),
         props: {
           handleDOMEvents: {
-            compositionstart: (view) => {
-              if (!view.state.selection.empty) {
+            compositionstart: (view, e) => {
+              // e.data.length > 0: テキストを選択してIME入力・変換して未確定の状態で入力を開始すると未確定のテキストが消えてしまう問題の対処
+              if (!view.state.selection.empty && e.data.length > 0) {
                 editor.commands.deleteSelection();
               }
               return false;
